60ed24032c6a35ff80a5b7d9dcae58a439922d02
Hi-lone <884673403@qq.com>
2025-06-02 14:51:49 +0800

    feat: 初始化项目

---
+++ index.js
@@ -0,0 +1,14 @@
+export function add(a, b) {
+  console.log(444444)
+  console.log(11122223333332321313)
+  console.log(11122223333332321313)
+  console.log(11122223333332321313)
+  console.log(11122223333332321313)
+  console.log(11122223333332321313)
+
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+import { add, subtract } from './index.js';
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
